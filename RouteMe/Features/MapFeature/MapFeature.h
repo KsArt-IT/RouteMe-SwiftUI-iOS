f3c7e80ec2cb748b20f3c6031cc23e1dd42f2622
KsArT-IT <ksart.it@gmail.com>
2025-02-04 19:21:47 +0200

    feat: add MapFeature documentation and header file

---
+++ RouteMe/Features/MapFeature/MapFeature.h
@@ -0,0 +1,18 @@
+//
+//  MapFeature.h
+//  MapFeature
+//
+//  Created by KsArT on 04.02.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapFeature.
+FOUNDATION_EXPORT double MapFeatureVersionNumber;
+
+//! Project version string for MapFeature.
+FOUNDATION_EXPORT const unsigned char MapFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapFeature/PublicHeader.h>
+
+
